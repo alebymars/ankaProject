319860c64666fdb8e046ef94f7b2a83f2ff5845e
Alexandr Voronkov <alebymars@ya.ru>
2023-05-20 21:09:12 +0700

    add authorization, add images, add new page and corrected styles

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const nextPage = (page) => {
+  window.location.href = page;
+};
+
+const auth = () => {
+  let pass = document.getElementById("password").value;
+  if (pass == "anka") {
+    console.log("Пароль =>", pass);
+    return nextPage("./src/index.html");
+  } else {
+    alert(`Пароль не подошёл, попробуйте еще раз.`);
+    return console.log("-", pass);
+  }
+};

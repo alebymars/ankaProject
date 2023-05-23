68f494fb4f7522f999b83c3fdcf178bf26105e18
Alexandr Voronkov <alebymars@ya.ru>
2023-05-23 22:33:06 +0700

    add index page, update styles and etc

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,38 @@
-const nextPage = (page) => {
-  window.location.href = page;
+const fetchWeather = async (city, day, apiKey) => {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=${day}`
+    );
+
+    let data = await response.json();
+
+    console.log(`data => ${JSON.stringify(data)}`);
+    localStorage.setItem("weather", JSON.stringify(data));
+
+    setTimeout(() => {
+      addWeatherInfo();
+    }, 2000);
+  } catch (e) {
+    console.log(`Error => ${e}`);
+  }
 };
 
-const auth = () => {
-  let pass = document.getElementById("password").value;
-  if (pass == "anka") {
-    console.log("Пароль =>", pass);
-    return nextPage("./src/index.html");
-  } else {
-    alert(`Пароль не подошёл, попробуйте еще раз.`);
-    return console.log("-", pass);
-  }
+const addWeatherInfo = () => {
+  const LS = JSON.parse(localStorage.getItem("weather"));
+  const infoWeather = Object.values(LS);
+
+  infoWeather.map((data) => {
+    const name = data.name;
+    console.log(`name => ${name}`);
+
+    // добавляем проверку, если undefined то ничего не возвращаем
+    if (name == undefined) {
+      return;
+    }
+
+    let main = document.querySelector(".main");
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = name;
+    main.appendChild(paragraph);
+  });
 };

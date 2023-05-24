d8918fd32ac784beebaebf5d4faa30beefd22fb9
Alexandr Voronkov <voronkov@vk.com>
2023-05-25 00:54:15 +0700

    corrected styles, fixed cards, add functions and etc

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
+// возвращаем выбранный селект (насколько дней выбираем погоду 1-7)
+const selectDay = () => {
+  const day = document.getElementById("selectDay").value;
+  // console.log(day);
+  return day;
+};
+
+// возвращаем город из поля поиска.
+const searchCity = () => {
+  const city = document.getElementById("searchCity").value;
+  // console.log(`city => ${city}`);
+  return city;
+};
+
+// делаем запрос api, получаем данные о погоде.
+// сохраняем данные в localStorage.
+// вызываем addWeatherInfo()
 const fetchWeather = async (city, day, apiKey) => {
   try {
     const response = await fetch(
@@ -30,9 +47,83 @@ const addWeatherInfo = () => {
       return;
     }
 
-    let main = document.querySelector(".main");
-    let paragraph = document.createElement("p");
+    const main = document.querySelector(".main");
+    const paragraph = document.createElement("p");
     paragraph.innerHTML = name;
     main.appendChild(paragraph);
   });
+};
+
+const addCard = () => {
+  const LS = JSON.parse(localStorage.getItem("weather"));
+  const infoWeather = Object.values(LS);
+
+  const hour = Object.values(infoWeather[2].forecastday[0].hour);
+  hour.map((hour) => {
+    console.log(hour.time);
+  });
+
+  // console.log(infoWeather[2].forecastday[0].hour);
+
+  // infoWeather.map((data, index) => {
+  //   // console.log(data.forecastday.hour.time);
+  //   const test = Object.values(data);
+  //   // test.map((info) => {
+  //   //   const other = Object.values(info);
+  //   //   // other.map((qa) => {
+  //   //   //   console.log(qa);
+  //   //   // });
+  //   //   // console.log(other);
+  //   // });
+  //   console.log(data);
+  // });
+
+  const cardDiv = `
+  <div class="cardUpInfo">
+      <p class="cityText">
+          Кемерово
+      </p>
+      <div class="cardUpInfoWeather">
+          <div class="tempAndConditionDiv">
+              <p class="tempText">
+                  +20°C
+              </p>
+              <p class="conditionText">
+                  Ясно
+              </p>
+          </div>
+          <img src="./images/sun.png" class="imageWeather" />
+      </div>
+  </div>
+  <div class="cardMiddleInfo">
+      <p>
+          влажность: 50%
+      </p>
+      <img src="./images/blob.png" class="imageIcon" />
+      <p>
+          ветер: 24м.с
+      </p>
+      <img src="./images/wind.png" class="imageIcon" />
+  </div>
+  <div class="cardDownInfo">
+      <div class="hourWeather">
+          <p class="timeText">
+              19:00
+          </p>
+          <p class="timeWeatherText">
+              +20
+          </p>
+      </div>
+  </div>
+  `;
+
+  const test = "<p>test</p>";
+
+  const main = document.getElementById("mainBlock");
+  const card = document.createElement("div");
+  card.className = "card";
+  card.id = "card";
+  card.innerHTML = cardDiv;
+
+  main.appendChild(card);
 };

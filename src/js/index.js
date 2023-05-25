d64b2952142249d8aa498be387814c069c2ffce8
Alexandr Voronkov <voronkov@vk.com>
2023-05-25 18:09:58 +0700

    fix card and etc

--- src/js/index.js
+++ src/js/index.js
@@ -55,8 +55,64 @@ const addWeatherInfo = () => {
 };
 
 const addCard = () => {
+  const cardDiv = `
+                <div class="cardUpInfo">
+                    <p id="cityText" class="cityText">
+                        Кемерово
+                    </p>
+                    <div class="cardUpInfoWeather">
+                        <div class="tempAndConditionDiv">
+                            <p class="tempText">
+                                +20°C
+                            </p>
+                            <p id="conditionText" class="conditionText">
+                                Ясно
+                            </p>
+                        </div>
+                        <img id="imgWeather" src="./images/sun.png" class="imageWeather" />
+                    </div>
+                </div>
+                <div class="cardMiddleInfo">
+                    <p id="humidity">
+                        влажность: 50%
+                    </p>
+                    <img src="./images/blob.png" class="imageIcon" />
+                    <p id="wind">
+                        ветер: 24км/ч
+                    </p>
+                    <img src="./images/wind.png" class="imageIcon" />
+                </div>
+                <div id="cardDownInfo" class="cardDownInfo">
+                    <div id="hourWeather" class="hourWeather">
+                        <p id="timeText" class="timeText">
+                            19:00
+                        </p>
+                        <p id="weatherText" class="weatherText">
+                            +20
+                        </p>
+                    </div>
+                </div>
+  `;
+
+  const main = document.getElementById("mainBlock");
+  const card = document.createElement("div");
+  card.className = "card";
+  card.id = "card";
+  card.innerHTML = cardDiv;
+
+  main.appendChild(card);
+};
+
+const updateCard = () => {
   const LS = JSON.parse(localStorage.getItem("weather"));
   const infoWeather = Object.values(LS);
+
+  console.log(infoWeather);
+
+  const frd = Object.values(infoWeather[2].forecastday);
+  frd.map((days) => {
+    // console.log(days);
+  });
 
   // родительский компонент карусели
   const cardDownInfo = document.getElementById("cardDownInfo");
@@ -114,53 +170,4 @@ const addCard = () => {
   cityText.innerHTML = infoWeather[0].name;
 
   console.log(infoWeather[0].name);
-
-  const cardDiv = `
-  <div class="cardUpInfo">
-      <p class="cityText">
-          Кемерово
-      </p>
-      <div class="cardUpInfoWeather">
-          <div class="tempAndConditionDiv">
-              <p class="tempText">
-                  +20°C
-              </p>
-              <p class="conditionText">
-                  Ясно
-              </p>
-          </div>
-          <img src="./images/sun.png" class="imageWeather" />
-      </div>
-  </div>
-  <div class="cardMiddleInfo">
-      <p>
-          влажность: 50%
-      </p>
-      <img src="./images/blob.png" class="imageIcon" />
-      <p>
-          ветер: 24м.с
-      </p>
-      <img src="./images/wind.png" class="imageIcon" />
-  </div>
-  <div class="cardDownInfo">
-      <div class="hourWeather">
-          <p class="timeText">
-              19:00
-          </p>
-          <p class="weatherText">
-              +20
-          </p>
-      </div>
-  </div>
-  `;
-
-  const test = "<p>test</p>";
-
-  const main = document.getElementById("mainBlock");
-  const card = document.createElement("div");
-  card.className = "card";
-  card.id = "card";
-  card.innerHTML = cardDiv;
-
-  main.appendChild(card);
 };

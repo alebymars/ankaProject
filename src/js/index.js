ca6a7c7282236c4d3dfb6eca4bfa7208671fde7b
Alexandr Voronkov <voronkov@vk.com>
2023-05-25 12:31:04 +0700

    create card weather

--- src/js/index.js
+++ src/js/index.js
@@ -18,7 +18,7 @@ const searchCity = () => {
 const fetchWeather = async (city, day, apiKey) => {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=${day}`
+      `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=${day}&lang=ru`
     );
 
     let data = await response.json();
@@ -26,9 +26,9 @@ const fetchWeather = async (city, day, apiKey) => {
     console.log(`data => ${JSON.stringify(data)}`);
     localStorage.setItem("weather", JSON.stringify(data));
 
-    setTimeout(() => {
-      addWeatherInfo();
-    }, 2000);
+    // setTimeout(() => {
+    //   addWeatherInfo();
+    // }, 2000);
   } catch (e) {
     console.log(`Error => ${e}`);
   }
@@ -58,25 +58,62 @@ const addCard = () => {
   const LS = JSON.parse(localStorage.getItem("weather"));
   const infoWeather = Object.values(LS);
 
+  // родительский компонент карусели
+  const cardDownInfo = document.getElementById("cardDownInfo");
+
+  // удаляем все из карусели с временем и погодой на каждый час
+  while (cardDownInfo.firstChild) {
+    cardDownInfo.removeChild(cardDownInfo.firstChild);
+  }
+
+  // мапим данные в карусель (время и погоду на каждыйы час)
   const hour = Object.values(infoWeather[2].forecastday[0].hour);
   hour.map((hour) => {
-    console.log(hour.time);
+    // генерируем родительский элемент
+    const hourWeather = document.createElement("div");
+    hourWeather.id = "hourWeather";
+    hourWeather.className = "hourWeather";
+
+    // генерируем дочерний элемент из карточки с каруселью (часы)
+    const timeText = document.createElement("p");
+    timeText.id = "timeText";
+    timeText.className = "timeText";
+
+    // генерируем дочерний элемент из карточки с каруселью (погода)
+    const weatherText = document.createElement("p");
+    weatherText.id = "weatherText";
+    weatherText.className = "weatherText";
+    hourWeather.innerHTML = `
+    <p class="timeText">
+      ${hour.time.split(" ")[1]}
+    </p>
+    <p class="weatherText">
+      ${hour.temp_c}°C
+    </p>
+    `;
+    // вставляем данные в родительский элемент (вставили дату и погоду)
+    cardDownInfo.appendChild(hourWeather);
   });
 
-  // console.log(infoWeather[2].forecastday[0].hour);
+  // получаем влажность по id
+  const humidity = document.getElementById("humidity");
+  // получаем ветер по id
+  const wind = document.getElementById("wind");
+  // получаем иконку погоды по id
+  const imgWeather = document.getElementById("imgWeather");
+  // получаем описание погоды по id (пасмупно, солнечно и т.д)
+  const conditionText = document.getElementById("conditionText");
+  // получаем название города по id
+  const cityText = document.getElementById("cityText");
 
-  // infoWeather.map((data, index) => {
-  //   // console.log(data.forecastday.hour.time);
-  //   const test = Object.values(data);
-  //   // test.map((info) => {
-  //   //   const other = Object.values(info);
-  //   //   // other.map((qa) => {
-  //   //   //   console.log(qa);
-  //   //   // });
-  //   //   // console.log(other);
-  //   // });
-  //   console.log(data);
-  // });
+  // меняем значения влажности, ветра, изображение у иконки погоды, описание погоды (одним словом), название города
+  humidity.innerHTML = `влажность: ${infoWeather[1].humidity}%`;
+  wind.innerHTML = `ветер: ${infoWeather[1].wind_kph}км/ч`;
+  imgWeather.src = `https:${infoWeather[1].condition.icon}`;
+  conditionText.innerHTML = infoWeather[1].condition.text;
+  cityText.innerHTML = infoWeather[0].name;
+
+  console.log(infoWeather[0].name);
 
   const cardDiv = `
   <div class="cardUpInfo">
@@ -110,7 +147,7 @@ const addCard = () => {
           <p class="timeText">
               19:00
           </p>
-          <p class="timeWeatherText">
+          <p class="weatherText">
               +20
           </p>
       </div>
